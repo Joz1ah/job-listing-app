04c7d4a90d7e94b88fe4c44322c9c7aecd126ca5
Mike <mike@akaza.io>
2024-12-27 10:39:41 +0800

    [FEAT] Added support for https on dev local

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,88 +1,123 @@
-import path from 'path'
-import express, { RequestHandler } from 'express'
-import compression from 'compression'
-import cookieParser from 'cookie-parser'
-import { ChunkExtractor } from '@loadable/server'
-
-import { csp, serverRenderer, nonce } from 'server/middlewares'
-import { IS_RENDER_TO_STREAM, SERVER_PORT } from 'server/constants'
-import { DIST_DIR, IS_DEV, SRC_DIR } from '_webpack/constants'
-import cors from 'cors'
-const { PORT = SERVER_PORT } = process.env
+import path from 'path';
+import express, { RequestHandler } from 'express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import { ChunkExtractor } from '@loadable/server';
+import { csp, serverRenderer, nonce } from 'server/middlewares';
+import { IS_RENDER_TO_STREAM, SERVER_PORT } from 'server/constants';
+import { DIST_DIR, IS_DEV, SRC_DIR } from '_webpack/constants';
+import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
 import dotenv from 'dotenv';
+
 dotenv.config();
-const allowedOrigins = ['http://localhost:8080','https://localhost:8080','https://akaza.xyz','https://akaza.io','https://app-sit.akaza.xyz'];
+
+const { PORT = SERVER_PORT } = process.env;
+
+const allowedOrigins = [
+  'http://localhost:8080',
+  'https://localhost:8080',
+  'https://akaza.xyz',
+  'https://akaza.io',
+  'https://app-sit.akaza.xyz',
+];
 
 const runServer = (hotReload?: () => RequestHandler[]): void => {
-  const app = express()
-  const statsFile = path.resolve('./dist/stats.json')
-  const chunkExtractor = new ChunkExtractor({ statsFile })
-
+  const app = express();
+  const statsFile = path.resolve('./dist/stats.json');
+  const chunkExtractor = new ChunkExtractor({ statsFile });
 
   app
-    .use(cors({
-      origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, origin); // Allow the request
-        } else {
-          callback(new Error('Not allowed by CORS')); // Reject the request
-        }
-      },
-      methods: ['GET', 'POST', 'OPTIONS'],
-      credentials: true,
-    }))
+    .use(
+      cors({
+        origin: (origin, callback) => {
+          if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, origin); // Allow the request
+          } else {
+            callback(new Error('Not allowed by CORS')); // Reject the request
+          }
+        },
+        methods: ['GET', 'POST', 'OPTIONS'],
+        credentials: true,
+      })
+    )
     .use(nonce)
     .use(csp)
     .use(express.json())
     .use(compression())
     .use(express.static(path.resolve(DIST_DIR)))
-    .use(cookieParser())
+    .use(cookieParser());
 
-    app
-    .options('*', (req, res) => {
-      const origin:any = req.headers.origin;
-    
-      if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', 'origin');
-        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-      }
-      res.status(200).end(); // Always respond with HTTP 200 OK
-    })
+  app.options('*', (req, res) => {
+    const origin: any = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+    }
+    res.status(200).end(); // Always respond with HTTP 200 OK
+  });
 
   if (IS_DEV) {
     if (hotReload != null) {
-      app.get('/*', [...hotReload()])
+      app.get('/*', [...hotReload()]);
     }
   } else {
     app.get('/sw.js', (_req, res) => {
-      res.sendFile(path.join(SRC_DIR, 'sw.js'))
-    })
+      res.sendFile(path.join(SRC_DIR, 'sw.js'));
+    });
   }
 
-  app.get('/*', serverRenderer(chunkExtractor))
+  app.get('/*', serverRenderer(chunkExtractor));
 
-  app.listen(PORT, () => {
-    console.log(
-      `App listening on port ${PORT}! (render to ${
-        IS_RENDER_TO_STREAM ? 'stream' : 'string'
-      })`
-    )
-  })
-}
+  if (!IS_DEV) {
+    // Development server - using HTTP
+    app.listen(PORT, () => {
+      console.log(
+        `App listening on http://localhost:${PORT} (render to ${
+          IS_RENDER_TO_STREAM ? 'stream' : 'string'
+        })`
+      );
+    });
+  } else {
+    // Production server - using HTTPS
+    const keyPath = path.resolve('./certs/key.pem');
+    const certPath = path.resolve('./certs/cert.pem');
+
+    if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
+      console.error('SSL key or certificate files are missing.');
+      process.exit(1);
+    }
+
+    const httpsOptions = {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    };
+
+    https.createServer(httpsOptions, app).listen(PORT, () => {
+      console.log(
+        `App listening on https://localhost:${PORT} (render to ${
+          IS_RENDER_TO_STREAM ? 'stream' : 'string'
+        })`
+      );
+    });
+  }
+};
 
 if (IS_DEV) {
-  ;(async () => {
+  (async () => {
     const { hotReload, devMiddlewareInstance } = await import(
       './middlewares/hotReload'
-    )
+    );
     devMiddlewareInstance.waitUntilValid(() => {
-      runServer(hotReload)
-    })
+      runServer(hotReload);
+    });
   })()
     .then(() => {})
-    .catch((er) => console.log(er))
+    .catch((er) => console.log(er));
 } else {
-  runServer()
+  runServer();
 }
