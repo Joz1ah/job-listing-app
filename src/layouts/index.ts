9aa8d09f5fad8eb47d6faa17c1337e51de41031f
Mike <mike@akaza.io>
2025-01-08 22:24:01 +0800

    [FEAT] Added about us

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -5,3 +5,4 @@ export { JobHunterHeader } from 'layouts/PageHeader/JobHunterHeader'
 export { Footer } from 'layouts/Footer/Footer'
 export { Footer as FooterEngagement } from 'layouts/Footer/Engagement/Footer'
 export { BaseMenu } from 'layouts/Menu/BaseMenu'
+export { DefaultLayout } from 'layouts/DefaultLayout/DefaultLayout';
