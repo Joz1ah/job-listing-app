fe5c8df80a36fcab2c82a1ec76bbad0bef03d036
Mike <mike@akaza.io>
2025-01-08 22:23:03 +0800

    [CHORE] Added test for quickfixes

---
+++ src/pages/test/Test.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import {AuthnetPaymentForm } from "components/payment/authnet/modal-payment-form/AuthnetForm";
+
+const Test : FC = () => {
+    return (
+        <>
+          <AuthnetPaymentForm/>
+        </>
+    )
+}
+
+export { Test }
