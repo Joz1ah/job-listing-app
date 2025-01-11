3555a3f273585f5f1aae56357b979b65f2314839
Mike <mike@akaza.io>
2025-01-12 01:07:37 +0800

    [FEAT] Updated intercom context to support guests

--- src/contexts/Intercom/IntercomContext.tsx
+++ src/contexts/Intercom/IntercomContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, ReactNode, useState, useEffect } from 'react';
-//import Intercom from "@intercom/messenger-js-sdk";
+import Intercom from "@intercom/messenger-js-sdk";
 
 // Define the context type with Intercom-related names
 interface IntercomContextType {
@@ -29,15 +29,25 @@ export const IntercomProvider: React.FC<IntercomProviderProps> = ({ children }) 
   const [userStatus, setUserStatus] = useState<string>('active'); // Example of user status
 
   // Set your APP_ID and correct user data
-  const APP_ID = "APP_ID";
+  const APP_ID = "acqnbntk";
   const current_user_email = "sartre@existentialist.com";
   const current_user_name = "Jean Paul Sartre";
   const current_user_id = "1940";
 
   // Initialize Intercom with user data on context update or component mount
   useEffect(() => {
+        Intercom({
+            app_id: APP_ID,
+            //name: current_user_name, // Full name
+            //email: current_user_email, // Email address
+            //user_id: current_user_id // current_user_id
+        })
 
     return () => {
+      // Cleanup Intercom when the component unmounts
+      if (window.Intercom) {
+        window.Intercom('shutdown');
+      }
     };
   }, [current_user_name, current_user_email, current_user_id, APP_ID]); // Dependencies for useEffect
 
