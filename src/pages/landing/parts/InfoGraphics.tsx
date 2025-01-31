06f2c4dce55217b5e2976df4e1ed4368896e86e4
aniezlouie <lodianiez@gmail.com>
2025-01-31 21:09:46 +0800

    WIP: adjust font size in pricing hero

--- src/pages/landing/parts/InfoGraphics.tsx
+++ src/pages/landing/parts/InfoGraphics.tsx
@@ -4,21 +4,22 @@ const InfoGraphics = () => {
   return (
     <div className={`${styles.infographic1}`}>
       <div className={`${styles["info-desc"]}`}>
-        <div className={`${styles["info-desc-wrapper"]}`}>
-          <div>
-            <div>
-              <label>$5 for a chance to</label> <label>#TakeBackYourTime</label>
+        <div
+          id="infographic-text"
+          className={`${styles["info-desc-wrapper"]} flex justify-end items-center`}
+        >
+          <div className="text-center text-base sm:text-lg lg:text-2xl">
+            <div className="mb-16">
+              $5 for a chance to{" "}
+              <span className="text-orange-500">#TakeBackYourTime</span>
             </div>
-          </div>
-          <div>
-            <div>No more commuting</div>
-            <div>No more hours of scrolling</div>
-            <div>More sleep</div>
-            <div>More money in your pockets</div>
-          </div>
-          <div>
-            <div>
-              <label>It's time to</label> <label>#TakeBackYourTime</label>
+            <div className="mb-16">No more commuting</div>
+            <div className="mb-16">No more hours of scrolling</div>
+            <div className="mb-16">More sleep</div>
+            <div className="mb-16">More money in your pockets</div>
+            <div className="mb-16">
+              It's time to{" "}
+              <span className="text-orange-500">#TakeBackYourTime</span>
             </div>
           </div>
         </div>
