8dbd8a0b5c7138530ed34f5f8955e0bc8e56c66f
Mike <mike@akaza.io>
2025-03-05 17:48:16 +0800

    [FIX] Force auth cookie removal

--- src/contexts/AuthContext/AuthContext.tsx
+++ src/contexts/AuthContext/AuthContext.tsx
@@ -31,11 +31,14 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     setIsAuthenticated(false);
     const hostname = window.location.hostname;
     const rootDomain = '.' + hostname.split('.').slice(-2).join('.');
-      
+    //Force Cookie Removal
+    Cookies.set('authToken', '', { expires: new Date(0), path: '/', domain: rootDomain });
+
+      /*
       [hostname, rootDomain].forEach(domain => {
         console.log(domain)
         Cookies.remove('authToken', { path: '/', domain });
-      });
+      });*/
     dispatch(akazaApiAccount.util.resetApiState());
   };
 
