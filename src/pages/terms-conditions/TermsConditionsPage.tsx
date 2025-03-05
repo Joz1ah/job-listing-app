ae0ca49d0d74042ab7393df02d6d350d05cd913f
Mike <mike@akaza.io>
2025-03-05 15:58:20 +0800

    [FEAT] Terms and Conditions
    This implements a webProxy to website Policies and added styling and script injections

--- src/pages/terms-conditions/TermsConditionsPage.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "node_modules/@types/react";
-
-const TermsConditionsPage : FC = () => {
-    return(
-        <></>
-    )
-}
-
-export { TermsConditionsPage }
--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -3,6 +3,7 @@ import * as API from "./api/";
 const routes = (app: any): void => {
   app.post("/api/contact-us-send-email", API.contactUsSendEmail);
   app.get("/api/notifications/stream", API.sseNotifications);
+  app.get("/api/webProxy", API.webProxy);
 };
 
 export default routes;
