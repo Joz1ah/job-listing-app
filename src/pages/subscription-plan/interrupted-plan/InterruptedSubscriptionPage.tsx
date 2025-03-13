66ebf79ccd91c0ac9534d97fed02d9c05bb084bd
joshua <joshua@akaza.io>
2025-03-13 20:14:33 +0800

    [FIX] icons and text im sign up interrupted

--- src/pages/subscription-plan/interrupted-plan/InterruptedSubscriptionPage.tsx
+++ src/pages/subscription-plan/interrupted-plan/InterruptedSubscriptionPage.tsx
@@ -22,6 +22,12 @@ import like_icon from "assets/subscription-plan-icons/thumbsup.svg?url";
 import infinity_icon from "assets/subscription-plan-icons/unli.svg?url";
 import lock_icon from "assets/subscription-plan-icons/lock.svg?url";
 import message_icon from "assets/subscription-plan-icons/chat.svg?url";
+import calender_icon_orange from "assets/subscription-plan-icons/calendar-orange.svg?url";
+import line_graph_icon_orange from "assets/subscription-plan-icons/line-graph-orange.svg?url";
+import like_icon_orange from "assets/subscription-plan-icons/like-orange.svg?url";
+import infinity_icon_orange from "assets/subscription-plan-icons/infinity-orange.svg?url";
+import lock_icon_orange from "assets/subscription-plan-icons/lock-orange.svg?url";
+import message_icon_orange from "assets/subscription-plan-icons/message-orange.svg?url";
 import { createAuthNetTokenizer, createAuthnetPaymentSecureData } from "services/authnet/authnetService";
 
 type PlanProps = {
@@ -357,48 +363,48 @@ const FreeTrial: React.FC<FreeTrialProps> = ({ onBack, onSelectPlan }) => {
       icon: <img src={sparkle_icon_green} className="w-4 h-4" alt="sparkle" />,
       text: "Perfect Match automation",
     },
-    { icon: <img src={like_icon} className="w-5 h-5" />, text: "Insights and Feedback" },
+    { icon: <img src={message_icon} className="w-5 h-5" />, text: "Live chat support" },
     { icon: <img src={card_icon} className="w-5 h-5" />, text: "No credit or debit card required" },
   ];
 
   const yearlyFeatures = [
     ...(isEmployer 
       ? [
-          { icon: <img src={infinity_icon} className="w-5 h-5" />, text: "Unlimited Interview Invites" },
-          { icon: <img src={calender_icon} className="w-5 h-5" />, text: "Up to 5 Job Listings" },
+          { icon: <img src={infinity_icon_orange} className="w-5 h-5" />, text: "Unlimited Interview Invites" },
+          { icon: <img src={calender_icon_orange} className="w-5 h-5" />, text: "Up to 5 Job Listings" },
         ]
       : [
-          { icon: <img src={calender_icon} className="w-5 h-5" />, text: "Send up to 3 Interview Invites" },
+          { icon: <img src={calender_icon_orange} className="w-5 h-5" />, text: "Send up to 3 Interview Invites" },
         ]
     ),
     {
       icon: <img src={sparkle_icon} className="w-4 h-4" alt="sparkle" />,
       text: "Perfect Match automation",
     },
-    { icon: <img src={like_icon} className="w-5 h-5" />, text: "Insights and Feedback" },
-    { icon: <img src={line_graph_icon} />, text: "Labour Market Insights" },
-    { icon: <img src={lock_icon} />, text: isEmployer ? "Exclusive Employer Resources" : "Exclusive Resources" },
-    { icon: <img src={message_icon} />, text: "Live chat support" },
+    { icon: <img src={like_icon_orange} className="w-5 h-5" />, text: "Insights and Feedback" },
+    { icon: <img src={line_graph_icon_orange} />, text: "Labour Market Insights" },
+    { icon: <img src={lock_icon_orange} />, text: isEmployer ? "Exclusive Employer Resources" : "Exclusive Resources" },
+    { icon: <img src={message_icon_orange} />, text: "Live chat support" },
   ];
 
   const monthlyFeatures = [
     ...(isEmployer 
       ? [
-          { icon: <img src={infinity_icon} className="w-5 h-5" />, text: "Unlimited Interview Invites" },
-          { icon: <img src={calender_icon} className="w-5 h-5" />, text: "Up to 5 Job Listings" },
+          { icon: <img src={infinity_icon_orange} className="w-5 h-5" />, text: "Unlimited Interview Invites" },
+          { icon: <img src={calender_icon_orange} className="w-5 h-5" />, text: "Up to 5 Job Listings" },
         ]
       : [
-          { icon: <img src={calender_icon} className="w-5 h-5" />, text: "Send up to 3 Interview Invites" },
+          { icon: <img src={calender_icon_orange} className="w-5 h-5" />, text: "Send up to 3 Interview Invites" },
         ]
     ),
     {
       icon: <img src={sparkle_icon} className="w-4 h-4" alt="sparkle" />,
       text: "Perfect Match automation",
     },
-    { icon: <img src={like_icon} className="w-5 h-5" />, text: "Insights and Feedback" },
-    { icon: <img src={line_graph_icon} />, text: "Labour Market Insights" },
-    { icon: <img src={lock_icon} />, text: isEmployer ? "Exclusive Employer Resources" : "Exclusive Resources" },
-    { icon: <img src={message_icon} />, text: "Live chat support" },
+    { icon: <img src={like_icon_orange} className="w-5 h-5" />, text: "Insights and Feedback" },
+    { icon: <img src={line_graph_icon_orange} />, text: "Labour Market Insights" },
+    { icon: <img src={lock_icon_orange} />, text: isEmployer ? "Exclusive Employer Resources" : "Exclusive Resources" },
+    { icon: <img src={message_icon_orange} />, text: "Live chat support" },
   ];
 
   const handleStartFreeTrial = () => {
