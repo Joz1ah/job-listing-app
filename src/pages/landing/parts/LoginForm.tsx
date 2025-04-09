bc440f3dd21f73ad181541ff69cbd850515f0590
joshua <joshua@akaza.io>
2025-04-09 19:50:30 +0800

    [FIX] quick fix for select component and login form

--- src/pages/landing/parts/LoginForm.tsx
+++ src/pages/landing/parts/LoginForm.tsx
@@ -98,7 +98,9 @@ const LoginForm = () => {
 
   const LoginSchema = Yup.object().shape({
     email: Yup.string().email("Invalid email").required("Email is required"),
-    password: Yup.string().required("Password is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters"),
   });
 
   return (
